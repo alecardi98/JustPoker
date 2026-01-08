906cf7e03fca6eb5b5f4e6492cdbfd8dab464050
alecardi98 <a.cardilloottavia@studenti.unibg.it>
2026-01-08 16:16:19 +0100

    Aggiornamenti di Game e ServerPoker

--- code/poker/src/main/java/THRProject/poker/Game.java
+++ code/poker/src/main/java/THRProject/poker/Game.java
@@ -1,5 +1,43 @@
 package THRProject.poker;
+import java.util.ArrayList;
 
+import player.Player;
+
+/*
+ * Classe che rappresenta lo stato del gioco
+*/
 public class Game {
 
+	private ArrayList<Player> turnazione; //rappresenta i player nell'ordine di gioco
+	private int piatto; //valore totale scommesso dai giocatori
+	
+	
+	public Game() {
+		turnazione = new ArrayList<Player>();
+		piatto = 0;
+	}
+
+	
+	/*
+	 * Getter & Setter 
+	 */
+	public ArrayList<Player> getTurnazione() {
+		return turnazione;
+	}
+
+
+	public void setTurnazione(ArrayList<Player> turnazione) {
+		this.turnazione = turnazione;
+	}
+
+
+	public int getPiatto() {
+		return piatto;
+	}
+
+
+	public void setPiatto(int piatto) {
+		this.piatto = piatto;
+	}
+	
 }
--- code/poker/src/main/java/THRProject/server/ServerPoker.java
+++ code/poker/src/main/java/THRProject/server/ServerPoker.java
@@ -1,7 +1,48 @@
 package THRProject.server;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import THRProject.poker.Game;
+import player.Player;
 
 public class ServerPoker {
 	
+	private static ServerPoker server;
+	private Game game;
 	
-
+	
+	private ServerPoker() {}
+	
+	
+	public void startServer() {
+		
+		
+		ArrayList<Player> players = new ArrayList<Player>();
+		
+		//il server ascolta i giocatori che si vogliono unire
+		//Player player = new Player();
+		
+		players.add(player);
+		
+		
+	}
+	
+	
+	public void startGame(ArrayList<Player> players) {
+		game = new Game();
+		game.setTurnazione(Collections.shuffle(players));
+		
+	}
+	
+	
+	/*
+	 * Getter & Setter 
+	*/
+	public static ServerPoker getServer() {
+		if(server == null)
+			server = new ServerPoker();
+			
+		return server;
+	}
+	
 }
