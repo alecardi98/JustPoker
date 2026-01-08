257d0cbc26c9b0e80bef883cfe1a3313279bedcd
Stefan Dolete <m.dolete@studenti.unibg.it>
2026-01-08 17:33:01 +0100

    Update Player.java

--- code/Poker/src/main/java/THRProject/player/Player.java
+++ code/Poker/src/main/java/THRProject/player/Player.java
@@ -19,6 +19,58 @@ public class Player {
 		
 	}
 	
+	public void partecipaPartita(){
+		
+	}
+	
+	public void esciPartita(){
+		
+	}
+	
+	public void partecipaInvito(int invito){
+		if(invito>=5 && invito<=100) {			//puntata d'invito tra 5 e 100 
+			punta(invito);
+		}else {
+			System.out.println("errore");
+		}
+	}
+	
+	public void nonPartecipa(){
+												//turno disabilitato per questa mano
+	}
+	
+	public void punta(int puntata){				// metodo per effettuare una puntata
+		if(fiches>=puntata) {
+			fiches-=puntata;
+		}else {
+			System.out.println("errore");
+		}
+			
+	}
+	
+	public void apri(int puntata){
+		punta(puntata);
+	}
+	
+	public void passa(){
+		
+	}
+	
+	public void lascia(){
+		
+	}
+	
+	public void vedi(){
+		
+	}
+	
+	public void cambio(int scelta){
+		
+	}
+	
+	public void servito(){
+		
+	}
 	
 	public String getUserName() {
 		return userName;
